beaf7d4f6713a44fedc1201267ceb6d8b492667f
Ines Melounou <imelouno@utbm.fr>
2019-11-15 11:43:16 +0100

    increment js

--- src/app/counter.service.ts
+++ src/app/counter.service.ts
@@ -11,34 +11,38 @@ import { ActionCableService, Channel } from 'angular2-actioncable';
 export class CounterService {
   
   subscription: Subscription;
-  public positionCounter = [49, 72, 48];
 
   private counterObservable: Map<number, Observable<Counter>>= new Map(); //partage donnee objet qu'on peut retrourner + dico 
 
   constructor(private httpClient : HttpClient,  private cableService: ActionCableService ) {
   // Open a connection and obtain a reference to the channel
   const channel: Channel = this.cableService
-  .cable('wss://lp4asgadot.herokuapp.com/cable')
-  .channel('CountersChannel ', {});
+                     .cable('wss://lp4asgadot.herokuapp.com/cable')
+                     .channel('CountersChannel', {});
 
     // Subscribe to incoming messages
     this.subscription = channel.received().subscribe(message => {
-        console.log(message)
+      console.log(message, this.counterObservable[message.id]);
+
+      if(this.counterObservable.has(message.id)) {
+        console.log("emit");
+        this.counterObservable[message.id].emit({id: message.id, name: message.name, value:message.value})
+      }
     });
 }
   reset() {
   }
 
 
-  increment(id: number): Observable<Counter>{
-    return this.httpClient.patch<Counter>("https://lp4asgadot.herokuapp.com/counters/"+id+".json", {});
+  increment(position: number) {
+    this.httpClient.patch<any>(".netlify/functions/increment", {counterId: position}).subscribe()
   }
 
   getCounter(id : number): Observable<Counter> {
     if (! this.counterObservable.has(id)) {
       this.counterObservable[id] = new EventEmitter<Counter>() 
     }
-    this.httpClient.get<Counter>("https://lp4asgadot.herokuapp.com/counters/"+this.positionCounter[id]+".json")
+    this.httpClient.get<Counter>("https://lp4asgadot.herokuapp.com/counters/"+id+".json")
                    .subscribe(counter =>  this.counterObservable[id].emit(counter))
     return this.counterObservable[id]
   }
