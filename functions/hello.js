f7e44116bce8219ff50ab2f68b286a545ae37b71
Ines Melounou <imelouno@utbm.fr>
2019-11-15 11:17:10 +0100

    hello {name}

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,12 @@
 exports.handler = function(event, context, callback) {
     console.log(JSON.stringify(event),JSON.stringify(context) )
+    const name = event.queryStringParameters.name || "World";
     callback(null, {
-      statusCode: 200,
+      statusCode: 2000,
       body: "Hello, World"
     });
+    return {
+        statusCode: 200,
+        body: `Hello, ${name}`
+      };
   };
