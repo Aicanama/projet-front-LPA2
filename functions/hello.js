c32a90765f8cb992fbb80eb7f4ea865b24d8010d
Ines Melounou <imelouno@utbm.fr>
2019-11-15 11:13:01 +0100

    console log

--- functions/hello.js
+++ functions/hello.js
@@ -1,4 +1,5 @@
 exports.handler = function(event, context, callback) {
+    console.log(JSON.stringify(event),JSON.stringify(context) )
     callback(null, {
       statusCode: 200,
       body: "Hello, World"
